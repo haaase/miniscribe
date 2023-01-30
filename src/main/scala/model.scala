956d379755e204a25cbcc894cf53cf655ae26245
Julian <github@julian-haas.de>
2023-01-31 00:37:14 +0100

    add model

---
+++ src/main/scala/model.scala
@@ -0,0 +1,27 @@
+package miniscribe.model
+
+case class Force(name: String, leader: Hero, warbands: Seq[Warband])
+
+case class Warband(hero: Hero, troopEntries: Seq[Troop])
+
+case class Hero(
+    name: String,
+    tier: Tier,
+    baseCost: Int,
+    equipment: Seq[Equipment]
+) extends Model
+
+case class Troop(name: String, baseCost: Int, equipment: Seq[Equipment])
+    extends Model
+
+trait Model:
+  val name: String
+  val equipment: Seq[Equipment]
+  val baseCost: Int
+  def getCost(): Int =
+    baseCost + equipment.filter(_.selected).map(_.cost).sum
+
+case class Equipment(cost: Int, selected: Boolean)
+
+enum Tier:
+  case Legend, Valour, Fortitude, Minor, Independent
