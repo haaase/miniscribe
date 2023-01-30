3f080652b6e54d54d0107975980b80b63c32ee1e
Julian <github@julian-haas.de>
2023-01-30 16:33:02 +0100

    working purely in scala js

--- build.sbt
+++ build.sbt
@@ -1,7 +1,9 @@
-enablePlugins(ScalaJSPlugin)
-enablePlugins(ScalaJSBundlerPlugin)
 name := "miniScribe"
 scalaVersion := "3.2.1"
+
+enablePlugins(ScalaJSPlugin)
+enablePlugins(ScalaJSBundlerPlugin)
+enablePlugins(ScalablyTypedConverterPlugin)
 
 // This is an application with a main method
 scalaJSUseMainModuleInitializer := true
@@ -10,4 +12,5 @@ libraryDependencies += "com.softwaremill.sttp.client3" %%% "core" % "3.8.9"
 libraryDependencies += "org.scala-lang.modules" %%% "scala-xml" % "2.1.0"
 scalaJSLinkerConfig ~= { _.withModuleKind(ModuleKind.CommonJSModule) }
 
-npmDependencies in Compile += "@zip.js/zip.js" -> "~2.6.62"
+Compile / npmDependencies += "@zip.js/zip.js" -> "~2.6.62"
+useYarn := true
--- src/main/scala/playground.scala
+++ src/main/scala/playground.scala
@@ -1,95 +1,54 @@
-import scala.util.Using
-import scala.util.Try
 import scala.util.Failure
 import scala.util.Success
 import sttp.client3._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js
 import js.annotation._
-import scala.collection.mutable
-// import js.annotation._
-
-// @js.native
-// @JSImport("https://deno.land/x/zipjs/index.js", "TextWriter")
-// object TextWriter = js.Dynamic
-
-// @js.native
-// @JSImport("https://deno.land/x/zipjs/index.js", "BlobReader")
-// object BlobReader = js.Dynamic
+import typings.zipJsZipJs.mod.{BlobReader, Entry, TextWriter, ZipReader}
+import scala.concurrent.Future
+import org.scalajs.dom.Blob
+import js.JSConverters._
+import scala.scalajs.js.typedarray._
 
 @main
 def main() =
   // get index
-  // val backend = FetchBackend()
+  val backend = FetchBackend()
 
-  // val mesbgRoot =
-  //   uri"http://localhost:8080/http://battlescribedata.appspot.com/repos/middle-earth"
-  // val mesbgIndex =
-  //   uri"$mesbgRoot/index.bsi"
+  val mesbgRoot =
+    uri"http://localhost:8080/http://battlescribedata.appspot.com/repos/middle-earth"
+  val mesbgIndex =
+    uri"$mesbgRoot/index.bsi"
 
-  // val indexZipped = basicRequest
-  //   .get(mesbgIndex)
-  //   .response(asByteArray)
-  //   .send(backend)
-// import {
-//   BlobReader,
-//   BlobWriter,
-//   TextReader,
-//   TextWriter,
-//   ZipReader,
-//   ZipWriter,
-// } from "https://deno.land/x/zipjs/index.js";
-  // unzip with nodejs module
+  val indexRequest: Future[Response[Either[String, Array[Byte]]]] = basicRequest
+    .get(mesbgIndex)
+    .response(asByteArray)
+    .send(backend)
 
-  object Zip {
-    @js.native
-    @JSImport("./ziptest.js", "doStuff")
-    def doStuff(): Unit = js.native
+  def decompressZip(zipFile: Uint8Array): Future[String] =
+    val zipFileBlob = new Blob(js.Array(zipFile))
+    val zipFileReader = new BlobReader(zipFileBlob)
+    val zipReader = new ZipReader(zipFileReader)
+    val helloWorldWriter = new TextWriter()
+    for
+      entries <- zipReader.getEntries().toFuture
+      firstEntry = entries.shift()
+      result <- firstEntry.getData_MEntry(helloWorldWriter).toFuture
+      _ <- zipReader.close().toFuture
+    yield result
+
+  val xmlString: Future[String] =
+    for
+      response <- indexRequest
+      zipFile = response.body match
+        case Left(error) =>
+          throw Exception(s"Failed to reach data repo: $error")
+        case Right(zipFile) =>
+          Uint8Array.from(zipFile.map(_.toShort).toJSArray)
+      result <- decompressZip(zipFile)
+    yield result
+
+  xmlString.onComplete {
+    case Success(value)     => println(value)
+    case Failure(exception) => println(s"Failed to acquire index: $exception")
   }
-
-  Zip.doStuff()
-  println("End of playground")
-  // for {
-  //   r <- indexZipped
-  // } {
-  //   r.body match
-  //     case Left(error) => println(error)
-  //     case Right(zip) =>
-  //       val mutableSeq: mutable.Seq[Byte] = mutable.Seq(zip.toSeq: _*)
-  //       val zipFileReader =
-  //         js.Dynamic.newInstance(js.Dynamic.global.BlobReader)(
-  //           mutableSeq.toJSArray
-  //         )
-  //       val helloWorldWriter =
-  //         js.Dynamic.newInstance(js.Dynamic.global.TextWriter)()
-  //       val zipReader = js.Dynamic.newInstance(js.Dynamic.global.ZipReader)(
-  //         zipFileReader
-  //       )
-  //       for {
-  //         firstEntry <- zipReader.getEntries().shift()
-  //         text <- firstEntry.getData(helloWorldWriter)
-  //         _ <- zipReader.close()
-  //       } {
-  //         println(text)
-  //       }
-  // }
-
-  // indexZipped.body match
-  //   case Left(error) => println(s"Cannot reach BS data repo: $error")
-  //   case Right(content) =>
-  //     Files.write(Path.of("index.zip"), content)
-  //     zipDecompress("index.zip", ".")
-
-  // // read xml from string
-  // Using[InputStream, Unit](Files.newInputStream(Path.of("index.xml"))) {
-  //   xmlStream =>
-  //     val xmlDoc = scala.xml.XML.load(xmlStream)
-  //     println(xmlDoc \ "dataIndexEntries")
-  //     val names =
-  //       for
-  //         entry <- xmlDoc \\ "dataIndexEntry"
-  //         name = entry \@ "dataName"
-  //         filePath = entry \@ "filePath"
-  //       yield (name, filePath)
-  //     println(names)
-  // }
