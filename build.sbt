6dacd32c29513d4ec328cc11c854401bdd58a933
Julian <github@julian-haas.de>
2023-02-02 23:45:31 +0100

    index retrieval workin

--- build.sbt
+++ build.sbt
@@ -9,6 +9,7 @@ enablePlugins(ScalablyTypedConverterPlugin)
 scalaJSUseMainModuleInitializer := true
 
 libraryDependencies += "com.softwaremill.sttp.client3" %%% "core" % "3.8.9"
+libraryDependencies += "org.gnieh" %%% "fs2-data-xml-scala" % "1.6.1"
 libraryDependencies += "org.scala-lang.modules" %%% "scala-xml" % "2.1.0"
 libraryDependencies += "de.tu-darmstadt.stg" %%% "rescala" % "0.32.0"
 libraryDependencies += "com.lihaoyi" %%% "scalatags" % "0.12.0"
@@ -16,6 +17,9 @@ libraryDependencies += "com.lihaoyi" %%% "scalatags" % "0.12.0"
 // js/ts dependencies
 Compile / npmDependencies += "@zip.js/zip.js" -> "~2.6.62"
 Compile / npmDependencies += "compression-webpack-plugin" -> "10.0.0"
+// disable sourcemaps
+Compile / fastOptJS / scalaJSLinkerConfig ~= { _.withSourceMap(false) }
+Compile / fullOptJS / scalaJSLinkerConfig ~= { _.withSourceMap(false) }
 
 // webpack config
 useYarn := true
--- src/main/scala/playground.scala
+++ src/main/scala/playground.scala
@@ -12,13 +12,17 @@ import org.scalajs.dom.Blob
 import typings.zipJsZipJs.mod.{BlobReader, Entry, TextWriter, ZipReader}
 import sttp.client3._
 import scalatags.JsDom.all._
+import fs2.{Fallible, Stream}
+import fs2.data.xml._
+import fs2.data.xml.dom._
+import fs2.data.xml.scalaXml._
 
 @main
 def main() =
   // get index
   val backend = FetchBackend()
   val mesbgRoot =
-    uri"http://localhost:8080/http://battlescribedata.appspot.com/repos/middle-earth"
+    uri"http://localhost:8090/http://battlescribedata.appspot.com/repos/middle-earth"
   val mesbgIndex =
     uri"$mesbgRoot/index.bsi"
   val indexRequest: Future[Response[Either[String, Array[Byte]]]] = basicRequest
@@ -40,7 +44,19 @@ def main() =
       _ <- zipReader.close().toFuture
     yield result
 
-  val xmlString: Future[String] =
+  // adapted from https://fs2-data.gnieh.org/documentation/xml/libraries/
+  def parseXML(xmlString: String): xml.Document =
+    Stream
+      .emits(xmlString)
+      .through(events[Fallible, Char]())
+      .through(documents)
+      .compile
+      .toList
+      .flatMap(docs => Right(docs.head))
+      .toTry
+      .get
+
+  val xmlDoc: Future[xml.Document] =
     for
       response <- indexRequest
       zipFile = response.body match
@@ -48,10 +64,20 @@ def main() =
           throw Exception(s"Failed to reach data repo: $error")
         case Right(zipFile) =>
           zipFile
-      result <- unzipTxtFile(zipFile)
-    yield result
+      xmlString <- unzipTxtFile(zipFile)
+    yield parseXML(xmlString)
 
-  xmlString.onComplete {
+  val dataIndex: Future[Seq[(String, String)]] =
+    for
+      doc <- xmlDoc
+      index = for
+        entry <- doc \\ "dataIndexEntry"
+        name = entry \@ "dataName"
+        filePath = entry \@ "filePath"
+      yield (name, filePath)
+    yield index
+
+  dataIndex.onComplete {
     case Success(value)     => println(value)
     case Failure(exception) => println(s"Failed to acquire index: $exception")
   }
